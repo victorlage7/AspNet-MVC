18467a668576cc102357eb5bd6556b487a2d7b33
Paulo Victor <victorlage7@gmail.com>
2020-01-08 21:32:34 -0300

    Trabalhando com Rotas

--- CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -11,6 +11,7 @@ namespace CaelumEstoque.Controllers
     public class ProdutoController : Controller
     {
         // GET: Produto
+        [Route("produtos", Name = "ListaProdutos")]
         public ActionResult Index()
         {
             ProdutosDAO dao = new ProdutosDAO();
@@ -50,7 +51,7 @@ namespace CaelumEstoque.Controllers
             }
         }
 
-
+        [Route("produtos/{id}", Name = "VisualizaProduto")]
         public ActionResult Visualiza(int id) 
         {
             ProdutosDAO dao = new ProdutosDAO();
